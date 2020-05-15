128864f9ba99165fba487aaa88fd084b0e6effb8
supun desktop <supun.chamikara@rocketmail.com>
2020-05-15 14:10:03 +0530

    windows

--- src/components/streams/stream-create.component.jsx
+++ src/components/streams/stream-create.component.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 const StreamCreate = () => {
-  return (<div>
-    Stream create
-  </div>);
+  return <div>Stream Create</div>;
 };
 
 export default StreamCreate;
+
+// windows comment
