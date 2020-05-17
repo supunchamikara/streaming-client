73b427e8eea7a527ad1d01cb9d015c5a66bbbb75
supun desktop <supun.chamikara@rocketmail.com>
2020-05-17 22:07:35 +0530

    object based reducers

--- src/components/streams/stream-create.component.jsx
+++ src/components/streams/stream-create.component.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
   renderInput = ({ input, label, meta }) => {
-    const className = `field ${meta.error && meta.touched ? 'error' : '' }`;
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
     return (
       <div className={className}>
         <label>{label}</label>
@@ -11,7 +13,7 @@ class StreamCreate extends React.Component {
         {this.handleError(meta)}
       </div>
     );
-  }
+  };
 
   handleError({ touched, error }) {
     if (touched && error) {
@@ -23,9 +25,9 @@ class StreamCreate extends React.Component {
     }
   }
 
-  onSubmit(formValues) {
-    console.log(formValues);
-  }
+  onSubmit = formValues => {
+    this.props.createStream(formValues);
+  };
 
   render() {
     return (
@@ -57,6 +59,8 @@ const validate = (formValues) => {
   return errors;
 };
 
-export default reduxForm({ form: "streamCreate", validate })(StreamCreate);
+const formWrapped = reduxForm({ form: "streamCreate", validate })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
 
 // windows comment
