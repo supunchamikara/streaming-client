e8fed451b6385223c98c4d0915395a5814469bdf
supun desktop <supun.chamikara@rocketmail.com>
2020-05-20 22:11:47 +0530

    all

--- src/actions/index.js
+++ src/actions/index.js
@@ -28,7 +28,7 @@ export const createStream = (formValues) => async (dispatch) => {
 };
 
 export const fetchStreams = () => async (dispatch) => {
-  const response = await streams.get("/strams");
+  const response = await streams.get("/streams");
   dispatch({ type: FETCH_STREAMS, payload: response.data });
 };
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from "./auth-reducer";
 import { reducer as formReducer } from "redux-form";
+import streamReducer from "./streamReducer";
 
 export default combineReducers({
   auth: authReducer,
   form: formReducer,
+  streams: streamReducer,
 });
